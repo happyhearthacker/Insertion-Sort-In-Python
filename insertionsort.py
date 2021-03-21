debfe1b7d4606bccddbef6b331c612dfce7386fa
happy_heart_hacker <ayush.singh8068@gmail.com>
2021-03-21 22:31:52 +0530

    Update insertionsort.py

--- insertionsort.py
+++ insertionsort.py
@@ -1,10 +1,12 @@
-a=list(input("Enter the elements of list":))
+a=list(input("Enter the elements of list:"))
+c=0
 for i in range(1,len(a)):
   key=a[i]
   j=i-1
   while j>=0 and a[j]>key:
     a[j+1]=a[j] 
-    j-+1
+    j-=1
   a[j+1]=key
   
 print("Sorted list:",a)
+print("no. of swapping:",c)
